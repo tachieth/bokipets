e5f3c4b81d1ac30e39dd4a682c2bee9b037d3871
FaisalAli <faisalali1901@gmail.com>
2023-10-07 20:31:38 +0530

    fixed the url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,19 @@ import { Button, HStack, Image, Link } from '@chakra-ui/react'
 export default function Header() {
   return (
     <HStack w="100%" justify="space-between" py="10px" px="20px">
-      <Link href="/">
-        <Image src="/images/Boki.png" alt="logo" w={{base: "120px", md: "160px"}} />
+      <Link href="https://www.boki.art/home" isExternal>
+        <Image src="/images/Boki.png" alt="logo" w={{ base: '120px', md: '160px' }} />
       </Link>
-      <Button bg="transparent" borderWidth="1px" borderColor="white" color="white" borderRadius="0" _hover={{ bg: "white", color: "black"}}>WALLET CONNECT</Button>
+      <Button
+        bg="transparent"
+        borderWidth="1px"
+        borderColor="white"
+        color="white"
+        borderRadius="0"
+        _hover={{ bg: 'white', color: 'black' }}
+      >
+        WALLET CONNECT
+      </Button>
     </HStack>
-  )
+  );
 }
